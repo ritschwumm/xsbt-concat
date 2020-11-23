2b5eacad57d4005512e002c3e8b8b7c13af50703
Herr Ritschwumm <ritschwumm@djini.de>
2020-11-23 10:44:50 +0100

    sbt 1.4.2, update dependencies, format

--- src/main/scala/xsbtConcat/ConcatProcessorPlugin.scala
+++ src/main/scala/xsbtConcat/ConcatProcessorPlugin.scala
@@ -18,41 +18,41 @@ object Import {
 	val concatName			= settingKey[String]("name of concatenated files, without suffix")
 	val concatBuildDir		= settingKey[File]("where to put concatenated sources")
 }
-	
+
 object ConcatProcessorPlugin extends AutoPlugin {
 	override val requires:Plugins		= AssetPlugin
-	
+
 	override val trigger:PluginTrigger	= allRequirements
-	
+
 	lazy val autoImport	= Import
 	import autoImport._
-	
+
 	override lazy val projectSettings:Seq[Def.Setting[_]]	=
-			Vector(
-				concatTypes			:= Seq(ConcatType("js"), ConcatType("css")),
-				concatName			:= "concat",
-				concatBuildDir		:= Keys.target.value / "concat",
-				
-				concatProcessor		:= {
-					Function chain (
-						concatTypes.value map singleProcessor(concatBuildDir.value, concatName.value)
-					)
-				}
-			)
-			
+		Vector(
+			concatTypes			:= Seq(ConcatType("js"), ConcatType("css")),
+			concatName			:= "concat",
+			concatBuildDir		:= Keys.target.value / "concat",
+
+			concatProcessor		:= {
+				Function chain (
+					concatTypes.value map singleProcessor(concatBuildDir.value, concatName.value)
+				)
+			}
+		)
+
 	def singleProcessor(buildDir:File, baseName:String)(typ:ConcatType):AssetProcessor = {
 		val filter	= GlobFilter(s"*.${typ.suffix}") && -DirectoryFilter
-		
+
 		(AssetProcessors selective filter) { input =>
 			val output	= buildDir / s"${baseName}.${typ.suffix}"
 			val files	= input map xu.pathMapping.getFile
-			
+
 			concatTexts(files, typ.header, typ.footer, output)
-			
+
 			Vector(output -> output.getName)
 		}
 	}
-	
+
 	def concatTexts(files:Seq[File], header:Option[String], footer:Option[String], to:File) {
 		val charset		= IO.utf8
 		val contents	= files map { IO read (_, charset) }
